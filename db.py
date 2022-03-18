d4fae0c6cd73787e4ef465a1662faa0b5fd1b167
Pururaj Singh Rajawat <pururajsrajawwat@gmail.com>
2022-03-19 00:16:30 +0530

    Removed some unwanted code.

--- db.py
+++ db.py
@@ -6,12 +6,6 @@ import re
 from Dictionary import verifiedTweet
 from RandomTweets import unverifiedTweet
 
-consumer_key = "PrClisT6ArX0u91a0Tr8FkLEd"
-consumer_secret = "c6tRq3XKpi4U5jB1Tavqjb4C3HDmmJ1L7jG6Xskk3CRtqIuAE7"
-access_token = "91092883-kaSrqgWvxQiSxSTsAHF7D34mTIBbmTA96W4sLYI9p"
-token_secret = "EOcnRNqxpfYSzws7aTz0WWbDYVFepDXLWxnbbDWk4DTr8"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAA%2B0aAEAAAAA3gerSEr%2BsRtUjaXfYK9iiOE1ZWM%3D4v6OoHklVcQobE37ZeWtKT6U7R1rkXIEhR9p1q21SzOo7A3SLg"
-
 # Connecting to Database
 try:
     cluster = MongoClient("mongodb+srv://puru:puru@cluster0.gh8mp.mongodb.net/TweetData?retryWrites=true&w=majority")
