a54240b70235e05333ae8d2be8b4afc6e0e27665
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2022-03-19 01:48:03 +0530

    Add file for news authenticity prediction

---
+++ pred.py
@@ -0,0 +1,18 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/chinhon/fake_tweet_detect"
+headers = {"Authorization": f"Bearer {'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'}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+def factcheck(input):
+    output = query({"inputs": input,})
+    false_score = output[0][0].get('score')
+    if(false_score>=0.6):
+      return "Fake"
+    elif(false_score<=0.4):
+      return "Not fake"
+    else:
+      return "Undetermined"
